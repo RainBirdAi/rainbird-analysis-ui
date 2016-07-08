45baf35376cb23c5896eb7a3b590e2056c56db61
lawrie <lawrence.russell@hotmail.co.uk>
2016-07-08 17:28:30 +0100

    AUI-2: Create first draft for why analysis

---
+++ layout.js
@@ -0,0 +1,351 @@
+/*
+    All code that is only concerned with the visual look of why analysis
+ */
+
+function layoutNode(node, nodeHolder) {
+    var nodeWidth = 200;
+    var subWidth = getWidestLength(nodeHolder.select('.subjectHolder'))+8;
+    var relWidth = getWidestLength(nodeHolder.select('.relationshipHolder'))+8;
+    var objWidth = getWidestLength(nodeHolder.select('.objectHolder'))+8;
+
+    nodeHolder.select('#ruleBlock')
+        .selectAll('text').each(function() {
+        var boundingBox = this.getBBox();
+        if (boundingBox.width > nodeWidth) {
+            nodeWidth = boundingBox.width;
+        }
+    });
+    if (nodeWidth < subWidth+relWidth+objWidth) {
+        nodeWidth = subWidth+relWidth+objWidth;
+    }
+    nodeWidth += 10;
+    nodeHolder.datum(function(d) {
+        d.width = nodeWidth;
+        return d;
+    });
+    var tripleNodeWidthDifference = nodeWidth-(subWidth+relWidth+objWidth);
+    var subPos = tripleNodeWidthDifference/2;
+    var relPos = subPos + subWidth + 4;
+    var objPos = relPos + relWidth + 4;
+    var nodeHeight = (node.rule ? node.rule.conditions.length * 25 + 94 : 84);
+    node.height = nodeHeight;
+
+    nodeHolder.select('rect')
+        .attr('width', nodeWidth + 16)
+        .attr('height', nodeHeight);
+
+    nodeHolder.select('.headerHolder')
+        .attr('transform', 'translate(4,4)');
+
+    nodeHolder.select('.rowHolder')
+        .attr('transform', 'translate(4,54)');
+
+    //Position triple set
+    nodeHolder.select('.rowHolder')
+        .select('rect')
+        .attr('width', nodeWidth + 8)
+        .attr('height', 40)
+        .attr('y', -15)
+        .attr('rx', 5);
+    nodeHolder.select('.rowHolder')
+        .selectAll('.holder')
+        .select('rect')
+        .attr('width', nodeWidth/3 - 4)
+        .attr('height', 32)
+        .attr('y', -11)
+        .attr('rx', 5);
+
+    //position triple rectangles
+    nodeHolder.select('.rowHolder')
+        .select('.subjectHolder')
+        .attr('transform', 'translate(' + subPos + ',0)')
+        .select('rect')
+        .attr('width', subWidth);
+    nodeHolder.select('.rowHolder')
+        .select('.relationshipHolder')
+        .attr('transform', 'translate(' + relPos + ',0)')
+        .select('rect')
+        .attr('width', relWidth);
+    nodeHolder.select('.rowHolder')
+        .select('.objectHolder')
+        .attr('transform', 'translate(' + objPos + ',0)')
+        .select('rect')
+        .attr('width', objWidth);
+
+    //position triple text
+    nodeHolder.select('.rowHolder')
+        .select('.subjectHolder')
+        .selectAll('text')
+        .attr('x', function() {
+            return subWidth/2 - d3.select(this).node().getBBox().width/2;
+        });
+    nodeHolder.select('.rowHolder')
+        .select('.relationshipHolder')
+        .selectAll('text')
+        .attr('x', function() {
+            return relWidth/2 - d3.select(this).node().getBBox().width/2;
+        });
+    nodeHolder.select('.rowHolder')
+        .select('.objectHolder')
+        .selectAll('text')
+        .attr('x', function() {
+            return objWidth/2 - d3.select(this).node().getBBox().width/2;
+        });
+
+    //Position rule block
+    nodeHolder.select('#ruleBlock')
+        .attr('transform', 'translate(8, 88)')
+        .select('rect')
+        .attr('height', nodeHeight - 86)
+        .attr('width', nodeWidth + 8)
+        .attr('x', '-4')
+        .attr('y', '-6')
+        .attr('rx', 5);
+    nodeHolder.select('#ruleBlock')
+        .selectAll('#ruleText')
+        .select('rect')
+        .attr('width', nodeWidth)
+        .attr('height', 20)
+        .attr('rx', 5);
+    nodeHolder.select('#ruleBlock')
+        .selectAll('text')
+        .attr('y', 16)
+        .attr('x', 5);
+    nodeHolder.select('#ruleBlock')
+        .selectAll('#ruleText')
+        .attr('transform', function(d, i) {
+            return 'translate(0, ' + i * 25 + ')';
+        })
+}
+
+function getWidestLength(node) {
+    var widest = 0;
+    node.selectAll('text')
+        .each(function(node) {
+            var boundingBox = this.getBBox();
+            if(boundingBox.width > widest) {
+                widest = boundingBox.width;
+            }
+        });
+    return widest;
+}
+
+function recalculate() {
+    var startYOffset = 43;
+    var endYOffset = 65;
+    var startTotalWidth = 32;
+    var endTotalWidth = 18;
+    var ruleGap = 26;
+    var getCertaintyWidth = function(node, inverted) {
+        var percentage = node.fact.certainty / 100;
+        return  inverted ? (1-percentage) * startTotalWidth : percentage * startTotalWidth;
+    };
+    var getSalienceWidth = function(node, inverted) {
+        var percentage = node.targetSalience / 100;
+        return  inverted ? (1-percentage) * endTotalWidth : percentage * endTotalWidth;
+    };
+
+    d3.selectAll('.underPath')
+        .each(function(path) {
+            var _this = d3.select(this);
+            _this.attr('d', '');
+
+            var targetY = path.parent.y - path.y + path.targetIndex * ruleGap + endYOffset;
+
+            var curve = new Bezier(path.width + 16, startYOffset, path.width + 50, startYOffset, path.parent.x - 50 - path.x,
+                targetY, path.parent.x - path.x, targetY);
+
+            //draw top curve
+            console.log(curve.points);
+            var endWidth = getSalienceWidth(path, false);
+            var leftCurve = function(c, i) { drawCurve(c, i+1, _this, true); };
+            var outline = curve.outline(getCertaintyWidth(path, false),1,endWidth,endWidth);
+            outline.curves.forEach(function(curve, index) {
+                leftCurve(curve, index)
+            });
+            drawCenterCurve(curve, 1, _this);
+        });
+
+    d3.selectAll('.overPath')
+        .each(function(path) {
+            var _this = d3.select(this);
+            _this.attr('d', '');
+
+            var targetY = path.parent.y - path.y + path.targetIndex * 22 + endYOffset;
+
+            var curve = new Bezier(
+                path.width + 16, startYOffset,
+                path.width + 50, startYOffset,
+                path.parent.x - 50 - path.x, targetY,
+                path.parent.x - path.x, targetY);
+
+            var curveInverted = new Bezier(
+                path.parent.x - path.x, targetY,
+                path.parent.x - 50 - path.x, targetY,
+                path.width + 50, startYOffset,
+                path.width + 16, startYOffset);
+
+            //draw top curve
+            var endWidth = getSalienceWidth(path, true);
+            var rightCurve = function(c, i) { drawCurve(c, i+1, _this, false); };
+            var outline = curve.outline(10,getCertaintyWidth(path, true),endWidth,endWidth);
+            outline.curves.forEach(function(curve, index) {
+                rightCurve(curve, index)
+            });
+            drawCenterCurve(curveInverted, 1, _this);
+        });
+
+    d3.selectAll('.pathText')
+        .attr('x', function(node) {
+            return (node.parent.x - node.x)/2 + 100;
+        })
+        .attr('y', function(node) {
+            return (node.parent.y - node.y)/2 + 85;
+        })
+        .text(function(d) {
+            return 'Certainty ' + d.fact.certainty + '%';
+        });
+}
+
+
+function drawCurve(curve, index, pathIn, leftSide) {
+    if (!index) {
+        index = 0;
+    }
+
+    var path;
+    if (pathIn) {
+        path = pathIn;
+    } else {
+        path = d3.select('svg').select('path');
+    }
+
+    var p = curve.points;
+    var currentPath = path.attr('d');
+
+    if ((leftSide && index < 5 && index > 0) || (!leftSide && index > 5)) {  //cull to left side of curve. Need to expand out to right side
+        if (p.length === 3) {
+            path.attr('d', function () {
+                return (currentPath ? currentPath : ' M ' + p[0].x + ',' + p[0].y ) +
+                    ' Q ' + p[1].x + ',' + p[1].y +
+                    ' ' + p[2].x + ',' + p[2].y;
+            });
+        }
+        else if (p.length === 4) {
+            path.attr('d', function () {
+                return (currentPath ? currentPath : ' M ' + p[0].x + ',' + p[0].y) +
+                    ' C ' + p[1].x + ',' + p[1].y +
+                    ' ' + p[2].x + ',' + p[2].y +
+                    ' ' + p[3].x + ',' + p[3].y;
+            });
+        }
+    }
+    path
+        .style('fill', leftSide ? 'rgba(255,100,100,0.5)' : 'rgba(0,0,0,0.5)');
+}
+
+function drawCenterCurve(curve, index, pathIn) {
+    var path = pathIn;
+    var p = curve.points;
+    var currentPath = path.attr('d');
+
+    if (p.length === 3) {
+        currentPath += ' L ' + p[2].x + ',' + p[2].y;
+        path.attr('d', function () {
+            return currentPath  +
+                ' Q ' + p[1].x + ',' + p[1].y +
+                ' ' + p[0].x + ',' + p[0].y;
+        });
+    }
+    else if (p.length === 4) {
+        currentPath += ' L ' + p[3].x + ',' + p[3].y;
+        path.attr('d', function () {
+            return currentPath +
+                ' C ' + p[2].x + ',' + p[2].y +
+                ' ' + p[1].x + ',' + p[1].y +
+                ' ' + p[0].x + ',' + p[0].y;
+        });
+    }
+}
+
+
+function addToColumn(node, depth) {
+    if (columns.length <= depth) {
+        columns.push([]);
+    }
+    columns[depth].splice(node.targetIndex, 0, node);
+}
+
+function updateColumnYPosition(depth) {
+    var yPos = 0;
+    var widestNode = 0;
+    columns[depth].forEach(function (node) {
+        if (node.width > widestNode) {
+            widestNode = node.width;
+        }
+    });
+    columns[depth].width = widestNode;
+    var xPos = getColumnX(depth);
+    columns[depth].forEach(function (node) {
+        node.y = yPos;
+        node.x = xPos;
+        yPos += node.height + 50;
+    });
+}
+
+function getColumnX(depth) {
+    var xPos = 0;
+    for(var i = 1; i <= depth; i++) {
+        xPos -= columns[i-1].width + 100;
+    }
+    return xPos;
+}
+
+function getIcon(id) {
+    var idType = id.slice(3,5);
+    switch (idType) {
+        case 'KF':
+            return 'glyphicon glyphicon-save';
+        case 'RF':
+            return 'glyphicon glyphicon-tasks';
+        case 'AF':
+            return 'glyphicon glyphicon-earphone';
+        case 'IF':
+            return 'glyphicon glyphicon-import';
+        case 'DF':
+            return 'glyphicon glyphicon-cloud-download';
+        default:
+            return 'glyphicon glyphicon-asterisk';
+    }
+}
+function getColor(id) {
+    var idType = id.slice(3,5);
+    switch (idType) {
+        case 'KF':
+            return 'lightYellow';
+        case 'RF':
+            return 'lightBlue';
+        case 'AF':
+            return 'lightRed';
+        case 'IF':
+            return 'glyphicon glyphicon-import';
+        case 'DF':
+            return 'glyphicon glyphicon-cloud-download';
+        default:
+            return 'glyphicon glyphicon-asterisk';
+    }
+}
+function getSource(source) {
+    switch (source) {
+        case 'km':
+            return 'Knowledge Map';
+        case 'answer':
+            return 'Ask';
+        case 'rule':
+            return 'Rule';
+        case 'datasource':
+            return 'Data-source';
+        default:
+            return 'break';
+    }
+}
--- whyAnalysis.js
+++ whyAnalysis.js
@@ -1,178 +1,273 @@
 var yolandaUrl = 'http://localhost:3100'; //'https://api.rainbird.ai';
 
-var backend = {
-    "WA:KF:abc1": {
-        factID: "WA:RF:ojaegojaeojaegjogae",                              // Here for completeness / debugging - probably won't be accessed by the code.
-        source: "rule",
-        fact: {
-            subject: {type: "Person", value: "Bob"},
-            relationship: {type: "speaks"},
-            object: {type: "Language", value: "English"},
-            certainty: 45
-        },
-        rule: {
-            description: "People generally speak the national language",
-            bindings: {
-                "%S": "Bob",
-                "%O": "English",
-                "%COUNTRY": "England"
-            },
-            conditions: [
-                {
-                    factID : "WA:KF:jgo3ug3ough35ough35ough",
-                    salience: 34,
-                    subject: "%*",
-                    relationship:"lives in",
-                    object: "%COUNTRY",
-                    wasMet: true
-                },
-                {
-                    factID : "WA:KF:jgo3ug3ough35ough35ough",
-                    salience: 34,
-                    subject: "%COUNTRY",
-                    relationship:"Has National Language",
-                    object: "%*",
-                    wasMet: true
-                }
-            ],
-            structureID: "WA:RI:ogsorgjsogjsgrgsr"                      // id of rule structure node below.
-        }
-    }
-};
+var columns = [];
 
-function getIcon(id) {
-    var idType = id.slice(3,5);
-    switch (idType) {
-        case 'KF':
-            return 'glyphicon glyphicon-save';
-        case 'RF':
-            return 'glyphicon glyphicon-tasks';
-        case 'AF':
-            return 'glyphicon glyphicon-earphone';
-        case 'IF':
-            return 'glyphicon glyphicon-import';
-        case 'DF':
-            return 'glyphicon glyphicon-cloud-download';
-        default:
-            return 'glyphicon glyphicon-asterisk';
-    }
+var drag = d3.behavior.drag()
+    .on("drag", function(d,i) {
+        //d.x += d3.event.dx;
+        d.y += d3.event.dy;
+        d.fact.subject.selected = true;
+        recalculate();
+        d3.select(this).attr("transform", function(d,i){
+            return "translate(" + [ d.x,d.y ] + ")"
+        })
+    })
+    .on("dragstart", function() {
+        d3.event.sourceEvent.stopPropagation();
+    });
+
+var pan = d3.behavior.drag()
+    .on("drag", function(d,i) {
+        d.x -= d3.event.dx;
+        d.y -= d3.event.dy;
+        d3.select(this).attr("viewBox", function(d,i){
+            return "" + [ d.x,d.y ] + " 700 600"
+        })
+    });
+
+function zoom() {
+    d3.select(this).select('g').attr('transform', 'scale(' + d3.event.scale +
+        ',' + d3.event.scale + ')');
 }
+var zoomer = d3.behavior.zoom()
+    .on("zoom", zoom);
+d3.select('svg')
+    .call(zoomer);
+
+var radius = 5;
+
+var conceptInstances = [];
+
+d3.select('#go').on('click', start);
 
 function start() {
 
-    getResults('WA:RS:example_resultset', function(data) {
-        d3.select('.explorer')
-            .append('div')
-            .text(data.goaltext)
-            .append('p')
-            .text('facts: ' + data.facts.length);
+    d3.select('svg')
+        .datum({x:0, y:0})
+        .call(pan);
 
-        data.facts.forEach(function (factID) {
-            getFact(factID, function(fact) {
-                addNode(fact, 0);
-            });
-        });
+    getFact(d3.select('#factID').property('value'), function(fact) {
+        d3.select('div')
+            .select('header')
+            .text(fact.fact.subject.value + ' ' + fact.fact.relationship.type + ' ' + fact.fact.object.value);
+
+        addNode(fact, 0);
     });
-
-    //d3.select('svg')
-    //    .append('g')
 }
 
-function addNode(node, depth) {
-    var nodeHolder = d3.select('#columnHolder')
-        .select('#column' + depth);
+function getConceptInstance(conceptInstance) {
+    for(var i = 0 ; i < conceptInstances.length; i++) {
+        if(conceptInstances[i].type === conceptInstance.type &&
+            conceptInstances[i].value === conceptInstance.value) {
+            return conceptInstances[i];
+        }
+    }
+    return null;
+}
 
-    if (nodeHolder[0][0]) {
-
-    } else {
-        nodeHolder = d3.select('#columnHolder')
-            .append('div')
-            .attr('id','column' + depth);
+function addNode(node, depth, parent) {
+    addToColumn(node, depth);
+    if (!getConceptInstance(node.fact.subject)) {
+        node.fact.subject.selected = false;
+        conceptInstances.push(node.fact.subject);
+    }
+    if (!getConceptInstance(node.fact.relationship)) {
+        node.fact.relationship.selected = false;
+        conceptInstances.push(node.fact.relationship);
+    }
+    if (!getConceptInstance(node.fact.object)) {
+        node.fact.subject.object = false;
+        conceptInstances.push(node.fact.object);
     }
 
-    nodeHolder = nodeHolder
-        .append('div')
-        .attr('class', 'nodeHolder blue');
+    var nodeHolder = d3.select('g')
+        .append('g');
+
+    nodeHolder.datum(function() {
+        if (parent) {
+            node.parent = parent;
+        }
+        return node;
+    });
+    nodeHolder
+        .attr('transform', function(d) {
+                return 'translate(' + d.x + ',' + d.y + ')'
+            });
+
+    nodeHolder.call(drag);
+
+    nodeHolder
+        .append('rect')
+        .attr('rx', radius)
+        .attr('rt', radius);
+
+    var color;
+    switch(node.source) {
+        case 'rule':
+            color = 'blue';
+            break;
+        case 'datasource':
+            color = 'green';
+            break;
+        case 'answer':
+            color = 'red';
+            break;
+        default:
+            color = 'yellow';
+            break;
+    }
+    nodeHolder.attr('class', '.nodeHolder ' + color);
 
     var headerHolder = nodeHolder
-        .append('div')
+        .append('g')
         .classed('headerHolder', true);
     headerHolder
-        .append('div')
-        .text(node.fact.certainty + '%');
+        .append('text')
+        .attr('y', 15)
+        .text(node.fact.subject.value + ' ' +
+            node.fact.relationship.type + ' ' +
+            node.fact.object.value + ' '
+            + node.fact.certainty + '%');
     headerHolder
-        .append('div')
-        .text(node.source);
+        .append('text')
+        .attr('y', 30)
+        .text(getSource(node.source));
     headerHolder
-        .append('span')
+        .append('text')
         .attr('class', getIcon(node.factID));
 
-
-    var rowHolder = nodeHolder.append('div').classed('rowHolder', true);
+    var rowHolder = nodeHolder.append('g').classed('rowHolder', true);
+    rowHolder
+        .append('rect')
+        .classed('white', true);
     var subjectHolder = rowHolder
-        .append('div')
+        .append('g')
+        .attr('transform', 'translate(0, 0)')
         .attr('class', 'subjectHolder holder');
     subjectHolder
-        .append('div')
+        .append('rect')
+        .classed(getColor(node.factID), true);
+        /*.on('mouseenter', function(d) {
+            getConceptInstance(node.fact.subject).selected = true;
+            console.log('in', conceptInstances[0]);
+            d3.selectAll('.subjectHolder')
+                .style('fill', style);
+        })
+        .on('mouseleave', function(d) {
+            getConceptInstance(node.fact.subject).selected = false;
+            console.log('out', conceptInstances[0]);
+            d3.selectAll('.subjectHolder')
+                .style('fill', style);
+        });*/  //TODO enable in a future version - allows highlighting of re-occuring instances of this conceptInstance
+    subjectHolder
+        .append('text')
         .text(node.fact.subject.type);
     subjectHolder
-        .append('div')
+        .append('text')
+        .attr('y', '15')
         .text(node.fact.subject.value);
 
     var relationshipHolder = rowHolder
-        .append('div')
+        .append('g')
+        .attr('transform', 'translate(60, 0)')
         .attr('class', 'relationshipHolder holder');
     relationshipHolder
-        .append('div')
+        .append('rect')
+        .classed(getColor(node.factID), true);
+    relationshipHolder
+        .append('text')
+        .attr('y', '7.5')
         .text(node.fact.relationship.type);
 
     var objectHolder = rowHolder
-        .append('div')
+        .append('g')
+        .attr('transform', 'translate(120, 0)')
         .attr('class', 'objectHolder holder');
     objectHolder
-        .append('div')
+        .append('rect')
+        .classed(getColor(node.factID), true);
+    objectHolder
+        .append('text')
         .text(node.fact.object.type);
     objectHolder
-        .append('div')
+        .append('text')
+        .attr('y', '15')
         .text(node.fact.object.value);
+
+    if (parent) {
+        nodeHolder.append('path')
+            .attr('class', 'underPath');
+        nodeHolder.append('path')
+            .attr('class', 'overPath');
+        nodeHolder.append('text')
+            .attr('class', 'pathText');
+    }
 
     if (node.rule) {
         addRuleBlock(node, nodeHolder, depth);
     }
+
+    layoutNode(node, nodeHolder);
+    updateColumnYPosition(depth);
+    nodeHolder.attr('transform', function(d) {
+        return 'translate(' + d.x + ',' + d.y + ')';
+    })
+}
+
+function style(d) {
+    return getConceptInstance(d.fact.subject).selected ? 'blue' : 'red';
 }
 
 function addRuleBlock(node, nodeHolder, depth) {
 
-    nodeHolder.append('div')
-        .text(node.rule.description);
+    /*nodeHolder.append('text')
+        .text(node.rule.description);*/  //TODO add this in when it becomes available
 
     var ruleBlock = nodeHolder
-        .append('div')
+        .append('g')
+        .attr('id', 'ruleBlock')
         .classed('ruleHolder', true);
 
-    node.rule.conditions.forEach(function(condition) {
+    ruleBlock.append('rect')
+        .style('fill', 'white');
+
+    node.rule.conditions.forEach(function(condition, i) {
+        if (!condition.factID) //TODO remove this when analysis is fixed
+            return;
+
         if (condition.expression) {
-            ruleBlock.append('div')
+            ruleBlock.append('g')
                 .text(condition.expression.text + condition.expression.value);
         } else {
-            var rowHolder = ruleBlock.append('div')
-                    .append('a')
-                    .text(' a');
+            var rowHolder = ruleBlock.append('g')
+                .attr('id', 'ruleText');
+            rowHolder
+                .append('rect')
+                .attr('class', getColor(node.factID));
+            rowHolder
+                .append('text')
+                .text(getReadableRuleText(node, condition));
 
-            rowHolder.on('mouseenter', function() {
-                d3.select(this).text(condition.subject + ' ' + condition.relationship + ' ' + condition.object);
+            rowHolder.select('text').on('mouseenter', function() {
+                d3.select(this).text(getRBLangRuleText(condition));
             });
-            rowHolder.on('mouseleave', function() {
-                d3.select(this).text(
-                    (condition.subject === '%*' ? node.rule.bindings['%S'] : node.rule.bindings[condition.subject]) + ' ' +
-                    condition.relationship + ' ' +
-                    (condition.object === '%*' ? node.rule.bindings['%O'] : node.rule.bindings[condition.object]));
+            rowHolder.select('text').on('mouseleave', function() {
+                d3.select(this).text(getReadableRuleText(node, condition));
             });
             var expanded = false;
             rowHolder.on('click', function() {
                 if (!expanded) {
-                    getFact('WA:KF:abc1', function(fact) {
-                        addNode(fact, depth+1);
+                    getFact(condition.factID, function(fact) {
+                        if (fact === "") {
+                            alert('dunno why this is empty');
+                        }
+                        else if (!expanded) {
+                            expanded = true;
+                            fact.targetIndex = ++i;
+                            fact.targetSalience = condition.salience ? condition.salience : 100;
+                            addNode(fact, depth + 1, node);
+                            recalculate();
+                        }
                     });
                 }
             });
@@ -180,17 +275,25 @@ function addRuleBlock(node, nodeHolder, depth) {
     });
 }
 
-function getFact(factID, callback) {
-    console.log('getFact', backend[factID]);
-    callback(backend[factID]);
+function getReadableRuleText(node, condition) {
+    /*return (condition.subject === '%*' ? node.rule.bindings['S'] : node.rule.bindings[condition.subject]) + ' ' +
+        condition.relationship + ' ' +
+        (condition.object === '%*' ? node.rule.bindings['O'] : node.rule.bindings[condition.object]);*/
+    return condition.subject + ' ' + condition.relationship + ' ' + condition.object;
 }
 
-/*function getFact(factID, callback) {
+function getRBLangRuleText(condition) {
+    return condition.subject + ' ' + condition.relationship + ' ' + condition.object;
+}
+
+
+
+function getFact(factID, callback) {
     $.ajax({
         type: 'GET',
         url: yolandaUrl + "/analysis/evidence/" + factID,
         success: function (data, status) {
-            console.log(data);
+            console.log('evidence', data);
             callback(data);
         },
         error: function (data, status) {
@@ -205,22 +308,7 @@ function getRule(ruleID, callback) {
         type: 'GET',
         url: yolandaUrl + "/analysis/ruleinfo/" + ruleID,
         success: function (data, status) {
-            console.log(data);
-            callback(data);
-        },
-        error: function (data, status) {
-            console.error(data, status);
-            alert('error: ' + data + ' ' + status);
-        }
-    });
-}*/
-
-function getResults(resultsID, callback) {
-    $.ajax({
-        type: 'GET',
-        url: yolandaUrl + "/analysis/resultset/" + resultsID,
-        success: function (data, status) {
-            console.log(data);
+            console.log('ruleinfo', data);
             callback(data);
         },
         error: function (data, status) {
@@ -230,4 +318,17 @@ function getResults(resultsID, callback) {
     });
 }
 
-start();
+function getResults(resultsID, callback) {
+    $.ajax({
+        type: 'GET',
+        url: yolandaUrl + "/analysis/resultset/" + resultsID,
+        success: function (data, status) {
+            console.log('resultset', data);
+            callback(data);
+        },
+        error: function (data, status) {
+            console.error(data, status);
+            alert('error: ' + data + ' ' + status);
+        }
+    });
+}
